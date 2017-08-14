109cbaa3602ef54ded9b6687889a51f5156e5764
07ASHISH <30887238+07ASHISH@users.noreply.github.com>
2017-08-14 12:20:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,20 @@ var  button= document.getElementById('counter');
 var counter =0;
 button.onclick = function() 
 {
-    counter = counter +1 ;
-    var span = document.getElementById('count');
+    var request = XMLHttpRequest();
+    request.onreadystatechange = function(){
+    // Process the server response here.
+    if(request.readystate===XMLHttpRequest.DONE)
+    {
+        if(request.status==200)
+        {
+         var counter = request.responseText;
+          var span = document.getElementById('count');
     span.innerHTML= counter.toString();
+        }
+    }
+};
+
+request.open('GET', 'http://ashishjha10001.imad.hasura-app.io/counter', true);
+request.send(null);
 };
